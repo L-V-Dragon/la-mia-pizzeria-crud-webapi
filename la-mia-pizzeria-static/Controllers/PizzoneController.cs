cac7b018013c89e001f4fe1d1087d3e606e5cca2
L-V-Dragon <viotti37@gmail.com>
2023-01-23 15:41:10 +0100

    Es completato

---
+++ la-mia-pizzeria-static/Controllers/PizzoneController.cs
@@ -0,0 +1,24 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using la_mia_pizzeria_static.Database;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzoneController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                List<Pizza> pizze = db.Pizze.ToList<Pizza>();
+
+                return Ok(pizze);
+            }
+        }
+    }
+}
